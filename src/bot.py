4b1da5ea544084ec31fb0c69dc842313f13ebbc9
Hepheir <hepheir@gmail.com>
2025-04-07 21:46:14 +0900

    feat: 좌석을 못 찾으면 조용히 있도록 변경

--- src/bot.py
+++ src/bot.py
@@ -42,16 +42,16 @@ async def on_ready():
         print(f'Scanning for {date}...')
         for movie in megabox.select_bokd_list(TARGET_MOVIE_NO, date).movie_form_list:
             if is_what_we_are_looking_for(movie):
-                lines.append(
-                    f'- {movie.play_date}({"월화수목금토일"[date.weekday()]}) {movie.play_start_time}-{movie.play_end_time} {movie.rest_seat_count}석 남아있어!')
+                lines.append(f'- {movie.play_date}({"월화수목금토일"[date.weekday()]}) {movie.play_start_time}-{movie.play_end_time} : `{movie.rest_seat_count}`석 남아있어!')
     if lines:
         lines.appendleft(f'삐용삐용!!! 우와아앗...!! 코엑스에 자리난 것 같아!')
         lines.append(f'')
         lines.append(f'서두르는게 좋을 것 같아, 행운을 빌어!')
         lines.append(f'(잔여석 중 약 4석은 장애인 전용석이라는 점에 유의해!)')
+        await channel.send('\n'.join(lines))
     else:
         lines.append(f'아직까진 코엑스에 자리가 없는 것 같아... ㅠㅠ')
-    await channel.send('\n'.join(lines))
+        # await channel.send('\n'.join(lines))
     await client.close()
 
 client.run(token=TOKEN)
