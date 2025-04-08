232db1e4eb4c7dd7b00ae6cdb4ee071b33f13271
Hepheir <hepheir@gmail.com>
2025-04-08 12:55:43 +0900

    feat: 자리를 못 찾아도 디버그 채널에 알림 전송하도록 기능 추가

--- src/bot.py
+++ src/bot.py
@@ -1,12 +1,19 @@
-import collections
 import datetime
 import os
+import typing
 import discord
 import megabox
 
 
+TIMEZONE = datetime.timezone(datetime.timedelta(hours=9), 'KST')
+
+DISCORD_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+DISCORD_CHANNEL_ID_WARN = 1358260652497440909  # 찾으면 알림 보낼 채널 (허밍 디스코드 진격거 쓰레드)
+DISCORD_CHANNEL_ID_INFO = 1359011032542085322  # 못찾아도 결과 보낼 채널
+
 TARGET_MOVIE_NO = "25008000"  # 진격거 완결편 극장판
 TARGET_THEATER_ID = 1351  # 코엑스 메가박스
+TARGET_REQUIRED_SEATS = 3 # 우리에게 필요한 최소 좌석 수
 TARGET_DATES = [
     datetime.date(2025, 4, 11),  # 금
     datetime.date(2025, 4, 12),  # 토
@@ -16,42 +23,54 @@ TARGET_DATES = [
 ]
 
 
-def is_what_we_are_looking_for(movie: megabox.MovieForm) -> bool:
-    if not movie.is_2d_mx4d:
-        return False
-    if not movie.branch_id == TARGET_THEATER_ID:
-        return False
-    available_seats = max(movie.rest_seat_count - 4, 0)  # 장애인석 제외
-    if available_seats < 3:
-        return False
-    return True
-
-
-TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-CHANNEL_ID = 1358260652497440909  # 허밍 디스코드 진격거 쓰레드
-
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
 
 @client.event
 async def on_ready():
-    channel = client.get_channel(CHANNEL_ID)
-    lines = collections.deque()
-    for date in TARGET_DATES:
-        print(f'Scanning for {date}...')
-        for movie in megabox.select_bokd_list(TARGET_MOVIE_NO, date).movie_form_list:
-            if is_what_we_are_looking_for(movie):
-                lines.append(f'- {movie.play_date}({"월화수목금토일"[date.weekday()]}) {movie.play_start_time}-{movie.play_end_time} : `{movie.rest_seat_count}`석 남아있어!')
-    if lines:
-        lines.appendleft(f'삐용삐용!!! 우와아앗...!! 코엑스에 자리난 것 같아!')
-        lines.append(f'')
-        lines.append(f'서두르는게 좋을 것 같아, 행운을 빌어!')
-        lines.append(f'(잔여석 중 약 4석은 장애인 전용석이라는 점에 유의해!)')
-        await channel.send('\n'.join(lines))
+    movies = find_all_movies(movie_no=TARGET_MOVIE_NO)
+    movies = list(filter(is_what_we_are_looking_for, movies))
+
+    if movies:
+        message = (
+            f'삐용삐용!!! 우와아앗...!! 코엑스에 자리난 것 같아!\n'
+            +'\n'.join(map(movie_to_string, movies))+'\n'+
+            f'\n'
+            f'서두르는게 좋을 것 같아, 행운을 빌어!\n'
+            f'(잔여석 중 약 4석은 장애인 전용석이라는 점에 유의해!)\n'
+        )
+        await client.get_channel(DISCORD_CHANNEL_ID_INFO).send(message)
+        await client.get_channel(DISCORD_CHANNEL_ID_WARN).send(message)
     else:
-        lines.append(f'아직까진 코엑스에 자리가 없는 것 같아... ㅠㅠ')
-        # await channel.send('\n'.join(lines))
+        message = (
+            f'아직까진 코엑스에 자리가 없는 것 같아... ㅠㅠ'
+            f' ({datetime.datetime.now().astimezone(TIMEZONE).strftime("%m.%d %H:%M")})'
+        )
+        await client.get_channel(DISCORD_CHANNEL_ID_INFO).send(message)
     await client.close()
 
-client.run(token=TOKEN)
+
+def find_all_movies(movie_no: str) -> typing.List[megabox.MovieForm]:
+    founds = []
+    for date in TARGET_DATES:
+        founds.extend(megabox.select_bokd_list(movie_no, date).movie_form_list)
+    return founds
+
+
+def is_what_we_are_looking_for(movie: megabox.MovieForm) -> bool:
+    if not movie.is_2d_mx4d:
+        return False
+    if not movie.branch_id == TARGET_THEATER_ID:
+        return False
+    available_seats = max(movie.rest_seat_count - 4, 0)  # 장애인석 제외
+    if available_seats < TARGET_REQUIRED_SEATS:
+        return False
+    return True
+
+
+def movie_to_string(movie: megabox.MovieForm) -> str:
+    return f'- {movie.play_date}({"월화수목금토일"[movie.play_date.weekday()]}) {movie.play_start_time}-{movie.play_end_time} : `{movie.rest_seat_count}`석 남아있어!'
+
+
+client.run(token=DISCORD_TOKEN)
